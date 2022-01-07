9f7438ed27a2593a479a758df446a51f985b4670
d-smith <doug.smith.mail@gmail.com>
2022-01-07 14:31:50 -0800

    added standard topic/queue example

--- src/main/java/com/myorg/FifoStack.java
+++ src/main/java/com/myorg/FifoStack.java
@@ -24,9 +24,11 @@ public class FifoStack extends Stack {
 
         //Notes - publishers to fifo topics set the message group
         // Things to try
-        // Can you subscribe a standard queue to a fifo topic?
-        // Can a fifo queue subscribe to multiple fifo topics?
-        // Message dedup
+        // Can you subscribe a standard queue to a fifo topic? ==> No
+        // Can you subscribe a fifo queue to a standard topic? ==> No
+        // Can a fifo queue subscribe to multiple fifo topics? ==> yes
+        // Message dedup ==> content-based dedup
+        // Message dedup vs content-based - AWS recommendations?
 
         Topic fifoTopic1 = Topic.Builder.create(this, "fifoTopic1")
                 .fifo(true)
@@ -41,6 +43,12 @@ public class FifoStack extends Stack {
                 .topicName("fifoTopic2")
                 .contentBasedDeduplication(true)
                 .build();
+
+        Topic topic3 = Topic.Builder.create(this, "topic3")
+                .displayName("topic3")
+                .topicName("topic3")
+                .build();
+
 
         Queue sub1queue = Queue.Builder.create(this, "sub1Queue")
                 .fifo(true)
@@ -72,6 +80,82 @@ public class FifoStack extends Stack {
                 .topic(fifoTopic2)
                 .build();
 
+
+        //Fifo queue subscribed to standard topic? ==> No
+        //Invalid parameter: Invalid parameter: Endpoint Reason: FIFO SQS Queues can not be subscribed to standard SNS topics (Service: AmazonSNS; Status Code: 400; Error Code:
+        //InvalidParameter; Request ID: 6cc2aee4-24c1-5a3e-8644-f4b576516a16; Proxy: null)
+
+
+        /*Subscription sub3 = Subscription.Builder.create(this, "sub3")
+                .protocol(SubscriptionProtocol.SQS)
+                .endpoint(sub1queue.getQueueArn())
+                .topic(topic3)
+                .build();*/
+
+
+        //Standard queue - sub to fifo?
+        //No -11:47:06 AM | CREATE_FAILED        | AWS::SNS::Subscription | q2subC6148ECF
+        //Invalid parameter: Invalid parameter: Endpoint Reason: Please use FIFO SQS queue (Service: AmazonSNS; Status Code: 400; Error Code: InvalidParameter; Request ID: 81de
+        //fdeb-b62f-5904-8e7f-a7baff55194c; Proxy: null)
+        /*Queue sub2queue = Queue.Builder.create(this, "sub2Queue")
+                .queueName("sub2q")
+                .visibilityTimeout(Duration.millis(30000))
+                .build();
+
+        QueuePolicy sub2QueuePolicy = QueuePolicy.Builder.create(this, "sub2QueuePolicy")
+                .queues(List.of(sub2queue))
+                .build();
+
+        sub2QueuePolicy.getDocument().addStatements(
+                PolicyStatement.Builder.create()
+                        .actions(List.of("sqs:SendMessage"))
+                        .principals(List.of(new ServicePrincipal("sns.amazonaws.com")))
+                        .resources(List.of(sub2queue.getQueueArn()))
+                        .build()
+        );
+
+        Subscription q2sub = Subscription.Builder.create(this, "q2sub")
+                .protocol(SubscriptionProtocol.SQS)
+                .endpoint(sub2queue.getQueueArn())
+                .topic(fifoTopic1)
+                .build();
+
+        CfnOutput.Builder.create(this, "s2QueueUrl")
+                .value(sub2queue.getQueueUrl())
+                .description("subscription 2 queue url")
+                .build();*/
+
+        //Standard queue subscribed to standard topic? Of course!
+        Queue sub2queue = Queue.Builder.create(this, "sub2Queue")
+                .queueName("sub2q")
+                .visibilityTimeout(Duration.millis(30000))
+                .build();
+
+        QueuePolicy sub2QueuePolicy = QueuePolicy.Builder.create(this, "sub2QueuePolicy")
+                .queues(List.of(sub2queue))
+                .build();
+
+        sub2QueuePolicy.getDocument().addStatements(
+                PolicyStatement.Builder.create()
+                        .actions(List.of("sqs:SendMessage"))
+                        .principals(List.of(new ServicePrincipal("sns.amazonaws.com")))
+                        .resources(List.of(sub2queue.getQueueArn()))
+                        .build()
+        );
+
+        Subscription q2sub = Subscription.Builder.create(this, "q2sub")
+                .protocol(SubscriptionProtocol.SQS)
+                .endpoint(sub2queue.getQueueArn())
+                .topic(topic3)
+                .build();
+
+        CfnOutput.Builder.create(this, "s2QueueUrl")
+                .value(sub2queue.getQueueUrl())
+                .description("subscription 2 queue url")
+                .build();
+
+
+        //Stack outputs
         CfnOutput.Builder.create(this, "fifoTopic1Arn")
                 .value(fifoTopic1.getTopicArn())
                 .description("fifoTopic1 arn")
@@ -82,9 +166,16 @@ public class FifoStack extends Stack {
                 .description("fifoTopic2 arn")
                 .build();
 
+        CfnOutput.Builder.create(this, "topic3Arn")
+                .value(topic3.getTopicArn())
+                .description("topic3 arn")
+                .build();
+
         CfnOutput.Builder.create(this, "s1QueueUrl")
                 .value(sub1queue.getQueueUrl())
                 .description("subscription 1 queue url")
                 .build();
+
+
     }
 }
